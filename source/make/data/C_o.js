02cc2ff2ee2c46cf10a06573654dd1debb392414
octoxalis <octoxalis@gmail.com>
2021-09-04 15:01:54 +0200

    initial

--- source/make/data/C_o.js
+++ source/make/data/C_o.js
@@ -103,7 +103,7 @@ const C_o =
   INS_REF_s:      '₂',        //: reference insert
   INS_QUO_s:      '₃',        //: quotation insert
   INS_TAB_s:      '₄',        //: table insert
-  
+
   INS_IMG_s:      '₉',        //: image insert
   INS_METHOD_o:
   {
@@ -129,7 +129,7 @@ const C_o =
 
   LEGEND_DELIM_s:  ' \u2219 ',   //: bullet
   INS_TRIGGER_s:    '\u25BE',    //: unicode ▾
-  //XXRANGE_DELIM_s:   ' \u2014 ',  //: long tiret
+  //XX RANGE_DELIM_s:   ' \u2014 ',  //: long tiret
 
 
   //=== DB ===
@@ -150,38 +150,6 @@ const C_o =
   BLOCK_PREAMBLE_s:  'preamble',
   BLOCK_SECTION_s:   'section_level',
   BLOCK_PARAGRAPH_s: 'paragraph',
-
-  //=== CSS ===
-  EXT_ARROW_s:     '2303',
-  INT_ARROW_s:     '2304',
-  DIM_SEPARATOR_s: '00D7',
-  LIST_MARKER_s:   '25AA',
-
-  
-  FULL_WIDTH_s:  '100vw',
-  FULL_HEIGHT_s: '100vh',
-  
-  FONT_BODY_s: `'Cantarell Regular', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Helvetica Neue, sans-serif`,
-  FONT_MONO_s: `'JetBrains Mono Light', system-ui, -apple-system, BlinkMacSystemFont, 'Ubuntu Mono', 'Liberation Mono', 'Courier New', Courier, monospace`,
-
-
-  //=== CSS constants to avoid calc(): can be changed in configure.js ===  
-  HUE_P_n:        CONF_o.HUE_P_n,
-  LUM_MODE_n:     CONF_o.LUM_MODE_n,
-  LUM_BASE_n:     CONF_o.LUM_BASE_n,
-  LUM_CONTRAST_n: CONF_o.LUM_CONTRAST_n,
-  LUM_FRONT_s:    CONF_o.LUM_FRONT_s,
-  LUM_BACK_s:     CONF_o.LUM_BACK_s,
-
-
-  PAD_1:  .25 * 1 + 'em',
-  PAD_2:  .25 * 2 + 'em',
-  PAD_3:  .25 * 3 + 'em',
-  PAD_4:  .25 * 4 + 'em',
-  PAD_6:  .25 * 6 + 'em',
-  PAD_8:  .25 * 8 + 'em',
-  PAD_12: .25 * 12 + 'em',
-  PAD_16: .25 * 16 + 'em',
 
 }
 
