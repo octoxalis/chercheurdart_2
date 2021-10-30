93d3d6ef3ffb98bf0d9bf9376336eb9184c8468f
octoxalis <octoxalis@gmail.com>
2021-10-30 11:19:31 +0200

    initial

--- source/make/data/C_o.js
+++ source/make/data/C_o.js
@@ -53,8 +53,8 @@ const C_o =
     MANIFEST_SRC:    '',
     SCRIPT_SRC_ELEM: '',
     OBJECT_SRC:      '',
-    SCRIPT_SRC:      `unsafe-eval`,
-    WORKER_SRC:      `unsafe-eval`,
+    SCRIPT_SRC:      `'unsafe-eval'`,
+    WORKER_SRC:      `'unsafe-eval'`,
     STYLE_SRC:       ``,
   },
 
